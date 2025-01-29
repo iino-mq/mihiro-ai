bc239167eefbff8f0de35722a4524723cc2ea11d
iino-mq <iino@mahoq.jp>
2025-01-29 23:49:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # StreamlitのSecretsからAPIキーを取得
 openai.api_key = st.secrets["OPENAI_API_KEY"]  
 
-st.title("The Slow - Mihiro")
+st.title("Mihiro - The Slow")
 
 # ユーザー入力を取得
 user_input = st.text_input("質問を入力してください")
@@ -14,7 +14,7 @@ if st.button("送信"):
     response = openai.chat.completions.create(  
         model="ft:gpt-4o-mini-2024-07-18:teammq::Av2dkXSy",
         messages=[
-            {"role": "system", "content": "あなたはミヒロ。フレンドリーなアドバイザーです。400文字程度で出力してください。"},
+            {"role": "system", "content": "あなたはミヒロさんです。400~800文字程度で出力してください。"},
             {"role": "user", "content": user_input}
         ]
     )
